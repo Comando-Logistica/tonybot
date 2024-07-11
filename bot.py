6193186b87cc2aef077a6de908c0588f9b7ccc3e
Victor Lopes <victorlopes3101@gmail.com>
2024-07-11 14:56:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,16 +33,38 @@ def handle_start(message):
         user_states.pop(message.from_user.id, None)
     else:
         bot.reply_to(message, f"Você não tem permissão para usar este comando. Seu ID é {message.from_user.id}")
-        print(f"ID {message.from_user.id} sem permissão!")
+        print(f"START | ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
-@bot.message_handler(commands=['ajuda'])
+@bot.callback_query_handler(func=lambda message: user_states.get(message.from_user.id, {}).get('state') == 'Ajuda')
 def handle_ajuda(message):
-    #print(f'Checking permission for user {message.from_user.id} to use "ajuda" command.')
     if has_permission(message.from_user.id, 'ajuda'):
-        ajuda_handler(bot, message)
+        print('111')
+        msg = '''
+    Olá, eu sou o Tony o assistente virtual da Comando!
+
+    Você pode selecionar a opção desejada clicando nos botões do menu:
+
+    Funcionando:
+
+    ⚫Placa - Reterna a documentação da placa
+    ⚫Motorista - retorna documentos do motorista (se enviada pelo RH ou responsável)
+    ⚫Codigo de Conduta - Retorna os codigo de contuda
+    ⚫Cartao CNPJ - Retorna o Cartao CNPJ
+    ⚫Apresentacao - retorna a Apresentação Institucional
+
+    Em progresso:
+
+    ⚫CTE/CTRC (Busca por CTE/CTRC) - Retorna os dados do Globus
+    '''
+
+        bot.reply_to(message.chat.id, msg)
+        #bot.send_message(message.chat.id, 'Escolha uma opção:', reply_markup=keyboard)
+        
+        #action = user_states.get(call.from_user.id, {}).get('action')
+        #ajuda_handler(bot, call)
         user_states.pop(message.from_user.id, None)
     else:
-        bot.reply_to(message, "Você não tem permissão para usar este comando.")
+        print(f"AJUDA | ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
 @bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get('state') == 'Aguardando placa')
 def handle_plate(message):
@@ -58,6 +80,7 @@ def handle_plate(message):
             user_states.pop(message.from_user.id, None)  # Resetar o estado do usuário
     else:
         bot.reply_to(message, "Você não tem permissão para usar este comando.")
+        print(f"PLACA | ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
 @bot.callback_query_handler(func=lambda call: user_states.get(call.from_user.id, {}).get('state') == 'Apresentação')
 def handle_apresentacao(call):
@@ -67,6 +90,7 @@ def handle_apresentacao(call):
         user_states.pop(call.from_user.id, None)
     else:
         bot.answer_callback_query(call.id, "Você não tem permissão para usar este comando.")
+        print(f"APRESENTAÇÃO | ID {call.from_user.id} sem permissão! {call.from_user.first_name}")
 
 
 @bot.callback_query_handler(func=lambda call: user_states.get(call.from_user.id, {}).get('state') == 'Codigo de Conduta')
@@ -77,6 +101,7 @@ def handle_conduta(call):
         user_states.pop(call.from_user.id, None) 
     else:
         bot.answer_callback_query(call.id, "Você não tem permissão para usar este comando.")
+        print(f"CONDUTA | ID {call.from_user.id} sem permissão! {call.from_user.first_name}")
 
 @bot.callback_query_handler(func=lambda call: user_states.get(call.from_user.id, {}).get('state') == 'Cartão CNPJ')
 def handle_cartao_cnpj(call):
@@ -88,7 +113,7 @@ def handle_cartao_cnpj(call):
         user_states.pop(call.from_user.id, None) 
     else:
         bot.answer_callback_query(call.id, "Você não tem permissão para usar este comando.")
-
+        print(f"CNPJ | ID {call.from_user.id} sem permissão! {call.from_user.first_name}")
 
 @bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get('state') == 'Aguardando CPF')
 def handle_cpf(message):
@@ -102,14 +127,22 @@ def handle_cpf(message):
             user_states.pop(message.from_user.id, None)  # Resetar o estado do usuário
     else:
         bot.reply_to(message, "Você não tem permissão para usar este comando.")
+        print(f"CPF | ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
-#@bot.message_handler(func=lambda message: True)
-#def handle_message(message):
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
     #print(f'Checking permission for user {message.from_user.id} to use "message" command.')
     #if has_permission(message.from_user.id, 'message'):
         #message_handler(bot, message)
     #else:
         #bot.reply_to(message, "Você não tem permissão para usar este comando.")
+    #print(f'Checking permission for user {message.from_user.id} to use "start" command.')
+    if has_permission(message.from_user.id, 'start'):
+        start_handler(bot, message)
+        user_states.pop(message.from_user.id, None)
+    else:
+        bot.reply_to(message, f"Você não tem permissão para usar este comando. Seu ID é {message.from_user.id}")
+        print(f"Qualquer| ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
@@ -118,6 +151,7 @@ def handle_callback_query(call):
         callback_query_handler(bot, call, user_states)
     else:
         bot.answer_callback_query(call.id, "Você não tem permissão para realizar esta ação.")
+        print(f"Callback| ID {call.from_user.id} sem permissão! {call.from_user.first_name}")
 
 # Novo Comando de Registro
 @bot.message_handler(commands=['registrar'])
@@ -136,6 +170,7 @@ def handle_register(message):
             bot.reply_to(message, "Formato incorreto. Use: /registrar user_id, nome, cargo")
     else:
         bot.reply_to(message, "Você não tem permissão para registrar usuários.")
+        print(f"Registrar| ID {message.from_user.id} sem permissão! {message.from_user.first_name}")
 
 def main():
     try:
